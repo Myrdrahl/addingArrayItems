a4a5192f038911213b1d5f3213ab53ced63c29ad
Even <even.juberg@ntnu.no>
2020-02-12 10:45:40 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+let displayZone = document.getElementById('displayZone')
+let addButton = document.getElementById('add')
+let displayButton = document.getElementById('display')
+let input = document.getElementById('userInput')
+let listOfItems = []
+
+function addItemsToArray () {
+  listOfItems.push(input.value)
+  console.log(listOfItems)
+}
+
+function showArray () {
+  for (let i = 0; i < listOfItems.length; i++) {
+    displayZone.innerText += ('Element ' + i + ' = ' + listOfItems[i])
+    displayZone.innerHTML += '</br>'
+  }
+}
+
+addButton.addEventListener('click', function () {
+  addItemsToArray()
+})
+
+displayButton.addEventListener('click', function (){
+  showArray()
+})
